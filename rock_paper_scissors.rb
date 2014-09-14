2d75d6356f09b603e3aecb40812d054a1dbdc909
Justin W. Stewart <stewartjustin3@gmail.com>
2014-09-14 07:34:05 -0400

    Commit from rock paper scissors

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,19 +1,22 @@
 # this application is a rock paper scissors game where the computer randoms selects a value after the user has entered input
 
 CASES = {"r"=>"rock", "p"=>"paper", "s"=>"scissors"}
-user_play = ""
-computer_play = ""
+
+
 
 loop do
   puts "*********************Welcome to Rock, Paper, Scissors******************************"
+  user_play = ""
   loop do
-    puts "Type r to select rock, type p to select paper, and s to select scissors"
+    puts 'Type "r" to select rock, type "p" to select paper, and "s" to select scissors'
     user_play = gets.chop.downcase
     if CASES.keys.include?(user_play)
+      puts "You selected: #{CASES[user_play]}"
       break
     end
+    puts "you selected #{user_play}, which is not a valid option"
   end  
-  puts "You selected: #{CASES[user_play]}"
+
   computer_play = CASES.keys.sample
   puts "The computer selected: #{CASES[computer_play]}"
   if(user_play =="r") && (computer_play == "r")
