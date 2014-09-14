ab862c34e7cc427f31b49cfbacc03acd7a2ceebd
Justin W. Stewart <stewartjustin3@gmail.com>
2014-09-14 08:02:17 -0400

    rps

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -2,8 +2,6 @@
 
 CASES = {"r"=>"rock", "p"=>"paper", "s"=>"scissors"}
 
-
-
 loop do
   puts "*********************Welcome to Rock, Paper, Scissors******************************"
   user_play = ""
@@ -18,25 +16,13 @@ loop do
   end  
 
   computer_play = CASES.keys.sample
-  puts "The computer selected: #{CASES[computer_play]}"
-  if(user_play =="r") && (computer_play == "r")
-    result = "DRAW.. we both selected rock"
-  elsif (user_play =="r") && (computer_play == "p")
-    result = "Sorry you lose, You select rock and computer has paper"
-  elsif (user_play =="r") && (computer_play == "s")
-    result = "Boo, you win.  you selected rock and I selected scissors.. rock SMASHES scissors"
-  elsif  (user_play =="p") && (computer_play == "r") 
-    result = "Boo.. you win paper covers rock"
-  elsif  (user_play =="p") && (computer_play == "p") 
-    result = "Draw :) we both selected paper"  
-  elsif  (user_play =="p") && (computer_play == "s") 
-    result = "Yeah I win scissors cuts paper"
-  elsif  (user_play =="s") && (computer_play == "r") 
-    result = "you lose, Rock SMASHES scissors"  
-  elsif  (user_play =="s") && (computer_play == "s") 
-    result = "Draw, we both selected scissors"        
-  elsif  (user_play =="s") && (computer_play == "p") 
-    result = "Boo..  scissors cut paper"    
+  puts  "The computer selected: #{CASES[computer_play]}"
+  if user_play == computer_play
+    result  = " DRAW!!"
+  elsif ((user_play =="s") && (computer_play == "r")) || ((user_play == 'r') && (computer_play =='s')) || ((user_play =='s') && (computer_play == 'p'))
+    result = " You won!"
+  else 
+    result = " Computer won :("    
   end 
 
   puts result 
